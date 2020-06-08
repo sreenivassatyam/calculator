ed7048374cb2bbc9c4872e372a4324658bb9b831
sreenivassatyam <66551334+sreenivassatyam@users.noreply.github.com>
2020-06-08 16:18:10 +0300

    removed unwanted + signs
    test

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -15,6 +15,7 @@ exports.calculate = function(req, res) {
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
+    'Mod':function(a,b){return 0},
   };
 
   // Determine the operation
