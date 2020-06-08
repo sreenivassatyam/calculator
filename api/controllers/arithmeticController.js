81633670b5a4fe945a3372fe45e4e071b4fc0953
sreenivassatyam <66551334+sreenivassatyam@users.noreply.github.com>
2020-06-08 16:25:23 +0300

    added comments
    test

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    // Need +sign before variable to addd. else it will be text concatination
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
